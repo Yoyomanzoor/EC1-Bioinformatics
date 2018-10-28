053531a73e3b71e220735cfa40fd937425932c43
Yoyo Manzoor <smanzoor@luc.edu>
2018-10-28 15:31:04 -0500

    Initialize

---
+++ blastcode.py
@@ -0,0 +1,13 @@
+import Bio
+
+# Convert supercontig fasta to one string sequence in txt file
+# file = open("supercontig.fasta","r").read()
+# contig = file.split("\n")
+# contig_filtered = [x for x in contig if ">Contig1" not in x]
+# f_contig = "".join(contig_filtered)
+# open("supercontig.txt", "w").write(f_contig)
+
+# Convert downloaded ORFs from NCBI to fasta file
+# f = open("__all.fa", "r").read()
+# open("orfs.fasta", "w").write(f)
+
